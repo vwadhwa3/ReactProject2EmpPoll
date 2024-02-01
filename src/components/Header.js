f2c5e23b358c74f57ad689f4fa67ab2d8ba51d9d
Varun Wadhwa <vwadhwa3@gmail.com>
2024-02-01 15:30:17 +0530

    header css done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 const Header = () => {
 return (
-    <div>
-        <ul>
-            <li>Home</li>
-            <li>Leaderboard</li>
-            <li>New</li>
-            <li>Image</li>
-            <li>Logout</li>
+    <div className="flex justify-between bg-blue-100 shadow-lg sm:bg-yellow-50">
+    <div className="Logo-container">
+        
+    </div>
+    <div className="flex items-center">
+        <ul className="flex p-4 m-4">          
+            <li className="px-4">Home</li>
+            <li className="px-4">Leaderboard</li>
+            <li className="px-4">New</li>
+            <li className="px-4">Image</li>
+            <li className="px-4">Logout</li>
         </ul>
+    </div>
     </div>
 )
 }
