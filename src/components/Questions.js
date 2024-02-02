afa6d5b859ce858b10f86d3fe549155c4317e7d4
Varun Wadhwa <vwadhwa3@gmail.com>
2024-02-02 14:32:38 +0530

    question page button working left

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,42 +1,69 @@
 import QuestionCard from './QuestionsCard'
 import { useDispatch ,useSelector } from "react-redux"; 
- 
+import {_getQuestions} from '../_DATA'
+import { useEffect, useState } from 'react';
 const Questions =()=>{
  
 const loggedInUser = useSelector(store=> store.user.user)
-console.log(loggedInUser)
-// const data =   _getUsers()
-// const userdata = Object.values(data).map((item)=>item)
-// const a = Object.values(userdata);
-// const b = JSON.parse(JSON.stringify(a));
-// console.log("a")
-// console.log(a)
-// console.log("b")
-// console.log(b)
+const [questionData, setQuestionData]=useState('')
+console.log(loggedInUser)  // currentUser
 
+console.log( "getting question data" );
+
+useEffect(()=>{
+    getQuestionData()
+},[])
+
+const getQuestionData = async() => { 
+    const data =  await _getQuestions()
+    console.log(data)
+    setQuestionData( await data)
+    
+  }
+
+  const a = Object.values(questionData);
+  console.log("converting quetions data")
+  console.log(a)
+  const b = JSON.parse(JSON.stringify(a));
+  console.log("b")
+  console.log(b)
+
+  b.sort((firstItem, secondItem) => secondItem.timestamp - firstItem.timestamp);
+  b.forEach(val => val.timestamp = new Date(val.timestamp).toLocaleString())
+ 
+  const newQuestions = b.filter(val=>{
+    return val.optionOne.votes.includes(loggedInUser) === false &&
+    val.optionTwo.votes.includes(loggedInUser )=== false
+  } )
+  console.log("newQuestions")
+  console.log(newQuestions)
+  const doneQuestions = b.filter(val=> val.optionOne.votes.includes(loggedInUser) === true ||
+  val.optionTwo.votes.includes(loggedInUser) === true)
+  console.log("doneQuestions")
+  console.log(doneQuestions)
     return (
         <div  >
             <div className="questionborder bg-gray-50">
                 <h3 className='font-bold'>New Question</h3>
                 <hr/>
                 <div className='cardflex'>
-                <QuestionCard/>
-                <QuestionCard/>
-                <QuestionCard/>
-                <QuestionCard/>
-                <QuestionCard/>
+                {
+                    newQuestions?.map(
+                        (x) =>  <QuestionCard  data={x}  /> 
+                    )
+                }                              
                 </div>
             </div>
             <div className="questionborder  bg-gray-50">
                 <h3 className='font-bold'>Answered Question</h3>
                 <hr/>
                 <div className='cardflex'>
-                <QuestionCard/>
-                <QuestionCard/>
-                <QuestionCard/>
-                <QuestionCard/>
-                <QuestionCard/>
-                <QuestionCard/>
+                    {
+                        doneQuestions?.map(
+                            (y)=> <QuestionCard data={y}/>
+                        )
+                    }
+                 
                 </div>
             </div>
         </div>
--- src/components/QuestionsCard.js
+++ src/components/QuestionsCard.js
@@ -1,9 +1,12 @@
-const QuestionCard =()=>{
+const QuestionCard =(props)=>{
+    console.log("props===")
+    console.log(props?.data)
+    const {author,timestamp }= props?.data
     return(
         <div className="cardflex bg-gray-200 ml-1 mb-1 mr-2">
             <div className="card text-center">
-                <span className="  font-bold">name</span><br/>
-                 <span>time</span><br/>
+                <span className="  font-bold">{author}</span><br/>
+                 <span>{timestamp}</span><br/>
                 <button className="p-2 mx-16 rounded-lg bg-black text-white shadow-lg"  >Show</button>
             </div>
         </div>        
